63212f05509c4e042970de771dc06350debfdada
hy <895350445@qq.com>
2020-02-19 21:27:47 +0800

    fix name issu

--- geecacahe/cache.go
+++ geecacahe/cache.go
@@ -6,16 +6,16 @@ import (
 )
 
 type cache struct {
-	mu          sync.Mutex
-	lru         *lru.Cache
-	cacaheBytes int64
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
 }
 
 func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.lru == nil {
-		c.lru = lru.New(c.cacaheBytes, nil)
+		c.lru = lru.New(c.cacheBytes, nil)
 	}
 	c.lru.Add(key, value)
 }
